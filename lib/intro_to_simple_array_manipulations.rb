59c78dec2635f6a80648626c769a1b4429885a38
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 14:45:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(array, string)
+  array.push(string)
+  return array
+end
+
+def using_unshift(arrray, string)
+  array.unshift(string)
+  return array
+end
+
+def using_pop(array)
+  array.pop
+  return array
+end
